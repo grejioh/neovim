e9928e58bf0df1b1788dae9fca3f37a69a9b6015
grejioh <grejioh@outlook.com>
2022-09-12 20:12:32 +0800

    use colorscheme catppuccin

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,4 +1,6 @@
-local colorscheme = "darkplus"
+vim.g.catppuccin_flavour = "frappe" -- latte, frappe, macchiato, mocha
+
+local colorscheme = "catppuccin"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
